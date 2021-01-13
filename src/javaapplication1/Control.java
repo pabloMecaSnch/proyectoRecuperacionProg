a37047c81897f6f4c5e1c678bbba29057706d4c5
pabloMecaSnch <60503126+pabloMecaSnch@users.noreply.github.com>
2021-01-13 09:49:23 +0100

    primer push

---
+++ src/javaapplication1/Control.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+import java.util.List;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author usuario
+ */
+public class Control {
+    
+    public BufferedReader br;
+    File file;
+    FileReader fr;
+    public Control(){
+        try {
+            fr = null;
+            file = new File(".\\animales.txt");
+            fr = new FileReader(file);
+            br = new BufferedReader(fr);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Control.class.getName()).log(Level.SEVERE, null, ex); 
+        }
+    }   
+    public ArrayList<String> lee(){
+        try {
+            ArrayList<String> zonas = new ArrayList<String>();
+            while(br.ready()){
+                String linea = br.readLine();
+                System.out.println(linea);
+                zonas.add(linea);
+            }
+             return zonas;
+        } catch (IOException ex) {
+            Logger.getLogger(Control.class.getName()).log(Level.SEVERE, null, ex);
+        }
+       return null;
+    }
+    
+    
+}
---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,113 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author usuario
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        FileOutputStream fos = null;
+        
+        DataOutputStream salida = null;
+        FileWriter fw = new FileWriter(".\\animales.txt",true);
+        
+        File file = new File(".\\animales.txt");
+        FileReader fr = new FileReader(file);
+        BufferedReader br = new BufferedReader(fr);
+        String linea;
+        int num = 0;
+        Map<Integer ,String> zonasEspaña = new HashMap<Integer, String>();
+        
+        try{
+            fos = new FileOutputStream(file,true);
+            salida = new DataOutputStream(fos);
+            
+            System.out.println("Introduce salir para acabar: ");
+            linea = sc.nextLine();
+            while(!linea.equals("salir")){
+                zonasEspaña.put(num, linea);
+                System.out.println("nuevo dato: "+ zonasEspaña.get(num));
+                num++;
+                String linea_espaciada = linea.concat("\n");
+                //fos.write(linea_espaciada.getBytes());
+                fw.write(linea_espaciada);
+                linea = sc.nextLine();
+                
+            }
+            try{
+             if (fos != null) {
+                    fos.close();
+                }
+                if (salida != null) {
+                    salida.close();
+                }
+                if(fw != null){
+                    fw.close();
+                }
+                } catch (IOException e) {
+                System.out.println(e.getMessage());                                                               
+            }
+           while(br.ready()){
+                System.out.println(br.readLine());
+                //buffer = new byte[40];
+            }
+            /*for (Map.Entry<Integer, String> entry : zonasEspaña.entrySet()) {
+                Integer key = entry.getKey();
+                String value = entry.getValue();
+                System.out.println(key+" "+value);
+            }*/
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(JavaApplication1.class.getName()).log(Level.SEVERE, null, ex);
+        }finally{
+            try{
+              
+             if (fos != null) {
+                    fos.close();
+                }
+                if (salida != null) {
+                    salida.close();
+                }
+                if(br != null){
+                    br.close();
+                }
+                if(fr != null){
+                    fr.close();
+                }
+                } catch (IOException e) {
+                System.out.println(e.getMessage());                                                               
+            }
+        }
+    }
+    
+}
